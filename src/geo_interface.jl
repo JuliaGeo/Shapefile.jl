ccec5bdadffabec11979ffe57b6d25d0b5fc6a05
Martijn Visser <mgvisser@gmail.com>
2019-09-20 17:01:22 +0200

    remove unneeded type parameters for geometry types
    According to the shapefile spec they are always Float64, so there is no need be more generic than that here.
    
    Also tightens the `SHAPETYPE` from `Dict{Int32, Any}` to `Dict{Int32, DataType}`.

--- src/geo_interface.jl
+++ src/geo_interface.jl
@@ -1,21 +1,21 @@
-GeoInterface.coordinates(obj::Point{T}) where {T <: Real} = T[obj.x, obj.y]
-GeoInterface.coordinates(obj::PointM{T}) where {T <: Real} = T[obj.x, obj.y]
-GeoInterface.coordinates(obj::PointZ{T}) where {T <: Real} = T[obj.x, obj.y, obj.z]
+GeoInterface.coordinates(obj::Point) = T[obj.x, obj.y]
+GeoInterface.coordinates(obj::PointM) = T[obj.x, obj.y]
+GeoInterface.coordinates(obj::PointZ) = T[obj.x, obj.y, obj.z]
 
-GeoInterface.coordinates(obj::MultiPoint{T}) where {T <: Real} =
+GeoInterface.coordinates(obj::MultiPoint) =
     Vector{T}[GeoInterface.coordinates(p) for p in obj.points]
-GeoInterface.coordinates(obj::MultiPointM{T,M}) where {T,M} =
+GeoInterface.coordinates(obj::MultiPointM) =
     Vector{T}[GeoInterface.coordinates(p) for p in obj.points]
-GeoInterface.coordinates(obj::MultiPointZ{T,M}) where {T,M} =
+GeoInterface.coordinates(obj::MultiPointZ) =
     Vector{T}[GeoInterface.coordinates(p) for p in obj.points]
 
-function GeoInterface.coordinates(obj::Polyline{T}) where T
+function GeoInterface.coordinates(obj::Polyline)
     npoints = length(obj.points)
     nparts = length(obj.parts)
     @assert obj.parts[nparts] <= npoints
     push!(obj.parts, npoints)
-    coords = Vector{Vector{T}}[
-        Vector{T}[
+    coords = Vector{Vector{Float64}}[
+        Vector{Float64}[
             GeoInterface.coordinates(obj.points[j+1])
             for j in obj.parts[i]:(obj.parts[i+1]-1)
         ]
@@ -25,13 +25,13 @@ function GeoInterface.coordinates(obj::Polyline{T}) where T
     coords
 end
 
-function GeoInterface.coordinates(obj::PolylineM{T,M}) where {T, M}
+function GeoInterface.coordinates(obj::PolylineM)
     npoints = length(obj.points)
     nparts = length(obj.parts)
     @assert obj.parts[nparts] <= npoints
     push!(obj.parts, npoints)
-    coords = Vector{Vector{T}}[
-        Vector{T}[
+    coords = Vector{Vector{Float64}}[
+        Vector{Float64}[
             GeoInterface.coordinates(obj.points[j+1])
             for j in obj.parts[i]:(obj.parts[i+1]-1)
         ]
@@ -41,13 +41,13 @@ function GeoInterface.coordinates(obj::PolylineM{T,M}) where {T, M}
     coords
 end
 
-function GeoInterface.coordinates(obj::PolylineZ{T,M}) where {T, M}
+function GeoInterface.coordinates(obj::PolylineZ)
     npoints = length(obj.points)
     nparts = length(obj.parts)
     @assert obj.parts[nparts] <= npoints
     push!(obj.parts, npoints)
-    coords = Vector{Vector{T}}[
-        Vector{T}[
+    coords = Vector{Vector{Float64}}[
+        Vector{Float64}[
             GeoInterface.coordinates(obj.points[j+1])
             for j in obj.parts[i]:(obj.parts[i+1]-1)
         ]
@@ -60,8 +60,8 @@ end
 # Only supports 2D geometries for now
 # pt is [x,y] and ring is [[x,y], [x,y],..]
 # ported from https://github.com/Turfjs/turf/blob/3d0efd96d59878f82c6d6baf8ed75695e0b6dbc0/packages/turf-inside/index.js#L76-L91
-function inring(pt::Vector{T}, ring::Vector{Vector{T}}) where T
-    intersect(i::Vector{T},j::Vector{T}) =
+function inring(pt::Vector{Float64}, ring::Vector{Vector{Float64}})
+    intersect(i::Vector{Float64},j::Vector{Float64}) =
         (i[2] >= pt[2]) != (j[2] >= pt[2]) && (pt[1] <= (j[1] - i[1]) * (pt[2] - i[2]) / (j[2] - i[2]) + i[1])
     isinside = intersect(ring[1], ring[end])
     for k in 2:length(ring)
@@ -71,18 +71,18 @@ function inring(pt::Vector{T}, ring::Vector{Vector{T}}) where T
 end
 
 # ported from https://github.com/Esri/terraformer-arcgis-parser/blob/master/terraformer-arcgis-parser.js#L168-L253
-function GeoInterface.coordinates(obj::Polygon{T}) where T
+function GeoInterface.coordinates(obj::Polygon)
     npoints = length(obj.points)
     nparts = length(obj.parts)
     @assert obj.parts[end] <= npoints
-    coords = Vector{Vector{Vector{T}}}[]
-    holes = Vector{Vector{Vector{T}}}()
+    coords = Vector{Vector{Vector{Float64}}}[]
+    holes = Vector{Vector{Vector{Float64}}}()
     push!(obj.parts, npoints)
         for i in 1:nparts
             ringlength = obj.parts[i+1] - obj.parts[i]
             ringlength < 4 && continue
             test = 0.0
-            ring = Vector{Vector{T}}()
+            ring = Vector{Vector{Float64}}()
             for j in (obj.parts[i]+1):(obj.parts[i+1]-1)
                 prev = obj.points[j]; cur = obj.points[j+1]
                 test += (cur.x - prev.x) * (cur.y + prev.y)
@@ -91,7 +91,7 @@ function GeoInterface.coordinates(obj::Polygon{T}) where T
             push!(ring, GeoInterface.coordinates(obj.points[obj.parts[i+1]]))
             @assert length(ring) == ringlength
             if test > 0 # clockwise
-                push!(coords, Vector{Vector{T}}[ring]) # create new polygon
+                push!(coords, Vector{Vector{Float64}}[ring]) # create new polygon
             else # anti-clockwise
                 push!(holes, ring)
             end
@@ -105,24 +105,24 @@ function GeoInterface.coordinates(obj::Polygon{T}) where T
                 break
             end
         end
-        push!(coords, Vector{Vector{T}}[hole]) # hole is not inside any ring; make it a polygon
+        push!(coords, Vector{Vector{Float64}}[hole]) # hole is not inside any ring; make it a polygon
     end
     coords
 end
 
 # copied from GeoInterface.coordinates{T}(obj::Polygon{T}) to match signature here
-function GeoInterface.coordinates(obj::PolygonM{T,M}) where {T, M}
+function GeoInterface.coordinates(obj::PolygonM)
     npoints = length(obj.points)
     nparts = length(obj.parts)
     @assert obj.parts[end] <= npoints
-    coords = Vector{Vector{Vector{T}}}[]
-    holes = Vector{Vector{Vector{T}}}()
+    coords = Vector{Vector{Vector{Float64}}}[]
+    holes = Vector{Vector{Vector{Float64}}}()
     push!(obj.parts, npoints)
         for i in 1:nparts
             ringlength = obj.parts[i+1] - obj.parts[i]
             ringlength < 4 && continue
             test = 0.0
-            ring = Vector{Vector{T}}()
+            ring = Vector{Vector{Float64}}()
             for j in (obj.parts[i]+1):(obj.parts[i+1]-1)
                 prev = obj.points[j]; cur = obj.points[j+1]
                 test += (cur.x - prev.x) * (cur.y + prev.y)
@@ -131,7 +131,7 @@ function GeoInterface.coordinates(obj::PolygonM{T,M}) where {T, M}
             push!(ring, GeoInterface.coordinates(obj.points[obj.parts[i+1]]))
             @assert length(ring) == ringlength
             if test > 0 # clockwise
-                push!(coords, Vector{Vector{T}}[ring]) # create new polygon
+                push!(coords, Vector{Vector{Float64}}[ring]) # create new polygon
             else # anti-clockwise
                 push!(holes, ring)
             end
@@ -145,24 +145,24 @@ function GeoInterface.coordinates(obj::PolygonM{T,M}) where {T, M}
                 break
             end
         end
-        push!(coords, Vector{Vector{T}}[hole]) # hole is not inside any ring; make it a polygon
+        push!(coords, Vector{Vector{Float64}}[hole]) # hole is not inside any ring; make it a polygon
     end
     coords
 end
 
 # copied from GeoInterface.coordinates{T}(obj::Polygon{T}) to match signature here
-function GeoInterface.coordinates(obj::PolygonZ{T,M}) where {T, M}
+function GeoInterface.coordinates(obj::PolygonZ)
     npoints = length(obj.points)
     nparts = length(obj.parts)
     @assert obj.parts[end] <= npoints
-    coords = Vector{Vector{Vector{T}}}[]
-    holes = Vector{Vector{Vector{T}}}()
+    coords = Vector{Vector{Vector{Float64}}}[]
+    holes = Vector{Vector{Vector{Float64}}}()
     push!(obj.parts, npoints)
         for i in 1:nparts
             ringlength = obj.parts[i+1] - obj.parts[i]
             ringlength < 4 && continue
             test = 0.0
-            ring = Vector{Vector{T}}()
+            ring = Vector{Vector{Float64}}()
             for j in (obj.parts[i]+1):(obj.parts[i+1]-1)
                 prev = obj.points[j]; cur = obj.points[j+1]
                 test += (cur.x - prev.x) * (cur.y + prev.y)
@@ -171,7 +171,7 @@ function GeoInterface.coordinates(obj::PolygonZ{T,M}) where {T, M}
             push!(ring, GeoInterface.coordinates(obj.points[obj.parts[i+1]]))
             @assert length(ring) == ringlength
             if test > 0 # clockwise
-                push!(coords, Vector{Vector{T}}[ring]) # create new polygon
+                push!(coords, Vector{Vector{Float64}}[ring]) # create new polygon
             else # anti-clockwise
                 push!(holes, ring)
             end
@@ -185,7 +185,7 @@ function GeoInterface.coordinates(obj::PolygonZ{T,M}) where {T, M}
                 break
             end
         end
-        push!(coords, Vector{Vector{T}}[hole]) # hole is not inside any ring; make it a polygon
+        push!(coords, Vector{Vector{Float64}}[hole]) # hole is not inside any ring; make it a polygon
     end
     coords
 end
