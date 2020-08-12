5f2bcf89f748e9ef4f5f5ae630a277f39c52a1c4
Fabian Greimel <fabgrei@gmail.com>
2020-08-12 17:55:05 +0200

    parts_polygon returns MultiPolygon
    more wip

--- src/basics.jl
+++ src/basics.jl
@@ -1,26 +1,68 @@
+using PolygonOps
+
 function parts_polygon(points::Vector{Point}, parts::Vector{Int32})
     exterior_pts = Vector{GB.Point{2, Float64}}[]
     interior_pts = Vector{GB.Point{2, Float64}}[]
     parts .+= 1
-    for i in 1:length(parts)
-        if i == 1 && length(parts) != 1
-            exterior_pts = collect(points[x] for x in parts[i]:parts[i+1]-1)
-        elseif length(parts) == 1
-            exterior_pts = points
-        elseif i < length(parts) && i > 1 
-            push!(interior_pts, collect(points[x] for x  in parts[i]:parts[i+1]-1))
+    push!(parts, length(points)+1)
+    # - split points into parts (rings)
+    # - determine if rings are exterior or interior (holes)
+    for i in 1:(length(parts)-1)
+        pts = collect(points[x] for x in parts[i]:parts[i+1]-1)
+        if hole(pts)
+            push!(interior_pts, pts)
         else
-            push!(interior_pts, collect(points[x] for x  in parts[i]:length(points)))
+            push!(exterior_pts, pts)
         end
     end
-    exterior = GB.LineString(exterior_pts)
-    if length(interior_pts) !=0
-        interiors = collect(GB.LineString(pts) for pts in interior_pts)
-        return GB.Polygon(exterior, interiors)
-    else
-        return GB.Polygon(exterior)
+    
+    if false # length(exterior_pts) == 1 # need GB.Polygon
+        exterior = GB.LineString(only(exterior_pts))
+        if length(interior_pts) != 0
+            interiors = collect(GB.LineString(pts) for pts in interior_pts)
+            return GB.Polygon(exterior, interiors)
+        else
+            return GB.Polygon(exterior)
+        end
+    else # need GB.MultiPolygon
+        # 1) match exteriors with interiors
+        if length(interior_pts) == 0
+            polygons = GB.Polygon.(exterior_pts)
+        else
+            @show eltype(interior_pts)
+            # for each interior return the index of containing exterior ring
+            i_exterior_matched = map(interior_pts) do int
+                print("! ")
+                pt_int = int[1]
+                
+                i_ext_matched = findfirst(eachindex(exterior_pts)) do i_ext
+                    ext = exterior_pts[i_ext]
+                    inpolygon(pt_int, ext) == 1 # TODO: function should return a Bool
+                end
+            end
+            # 2) for each exterior collect all associated interiors
+            polygons = map(enumerate(exterior_pts)) do (i_ext, ext)
+                interiors = GB.LineString.(interior_pts[i_exterior_matched .== i_ext])
+                exterior = GB.LineString(ext)
+                GB.Polygon(exterior, interiors)
+            end
+        end
+        
+        return GB.MultiPolygon(polygons)
     end
 end
+
+using ShiftedArrays
+# check if non-convex polygon is clockwise: 
+# https://stackoverflow.com/a/1165943/4984167
+function f(edge)
+    x1, y1 = edge[1]
+    x2, y2 = edge[2]
+    - (x2 - x1) / (y2 + y1)
+end
+edges(pts) = zip(pts, ShiftedArrays.circshift(pts, -1)) # ShiftedArrays.circshift is lazy
+clockwise(pts) = sum(f, edges(pts)) > 0
+hole(pts) = !clockwise(pts)
 
 function parts_polyline(points::Vector{Point}, parts::Vector{Int32})
     linestrings = GB.LineString{2,Float64,GB.Point{2,Float64}}[]
