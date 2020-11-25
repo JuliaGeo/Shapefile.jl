156226a250751a9492621460f375a090e9068de1
Martijn Visser <mgvisser@gmail.com>
2020-11-25 12:21:26 +0100

    minor change

--- src/Shapefile.jl
+++ src/Shapefile.jl
@@ -131,9 +131,10 @@ mutable struct Handle{T<:Union{<:GeoInterface.AbstractGeometry,Missing}}
     mrange::Interval
     shapes::Vector{T}
 end
+
 function Handle(path::AbstractString)
     open(path) do io
-        read(io, Shapefile.Handle)
+        read(io, Handle)
     end
 end
 
