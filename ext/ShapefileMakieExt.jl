7d245c88acc420bf8f054ddafa1b9194a5df72b5
Rafael Schouten <rafaelschouten@gmail.com>
2024-01-07 13:36:27 +0100

    Add a Makie extension (#103)
    * add Makie extension for plotting
    
    * add Makie extension
    
    * add Makie to targets
    
    * better extension name
    
    * fix extension path
    
    * stop supporting < 1.9

---
+++ ext/ShapefileMakieExt.jl
@@ -0,0 +1,22 @@
+module ShapefileMakieExt
+using GeoInterfaceMakie: GeoInterfaceMakie
+using Shapefile: Shapefile
+using Makie: Makie
+
+GeoInterfaceMakie.@enable Shapefile.AbstractShape
+GeoInterfaceMakie.@enable Shapefile.SubPolygon
+GeoInterfaceMakie.@enable Shapefile.LinearRing
+
+Makie.plottype(tbl::Shapefile.Table) = Makie.plottype(Shapefile.shapes(tbl))
+Makie.plottype(shp::Shapefile.Handle) = Makie.plottype(Shapefile.shapes(shp))
+
+for T in (Any, Union{Type{Any}, Type{<:Makie.AbstractPlot}}, Type{<:Makie.Poly}, Type{<:Makie.Lines}, Makie.PointBased)
+    @eval begin
+        Makie.convert_arguments(t::$T, tbl::Shapefile.Table) =
+            Makie.convert_arguments(t, Shapefile.shapes(tbl))
+        Makie.convert_arguments(t::$T, shp::Shapefile.Handle) =
+            Makie.convert_arguments(t, Shapefile.shapes(shp))
+    end
+end
+
+end
