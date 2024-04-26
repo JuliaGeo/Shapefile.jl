9bc48e89bfbf6d5e58b5d91ac818668048bc44df
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-04-25 20:48:51 -0400

    Allow Shapefile to read `.zip` files
    We should subscribe to the FileIO interface so we can deal with streaming files as well...
    
    Co-authored-by: David Gleich <dgleich@purdue.edu>

---
+++ ext/ShapefileZipFileExt.jl
@@ -0,0 +1,39 @@
+module ShapefileZipFileExt
+import ZipFile, Shapefile
+import Shapefile: _read_shp_from_zipfile
+function _read_shp_from_zipfile(zipfile)
+  r = ZipFile.Reader(zipfile)
+  # need to get dbx
+  shpdata, shxdata, dbfdata, prjdata = nothing, nothing, nothing, nothing
+  for f in r.files
+    fn = f.name
+    lfn = lowercase(fn)
+    if endswith(lfn, ".shp")
+      shpdata = IOBuffer(read(f))
+    elseif endswith(lfn, ".shx")
+      shxdata = read(f, Shapefile.IndexHandle)
+    elseif endswith(lfn, ".dbf")
+      dbfdata = Shapefile.DBFTables.Table(IOBuffer(read(f)))
+    elseif endswith(lfn, "prj")
+      prjdata = try
+        Shapefile.GeoFormatTypes.ESRIWellKnownText(Shapefile.GeoFormatTypes.CRS(), read(f, String))
+      catch
+        @warn "Projection file $zipfile/$lfn appears to be corrupted. `nothing` used for `crs`"
+        nothing 
+      end
+    end
+  end
+  close(r)
+  @assert shpdata !== nothing
+  shp = if shxdata !== nothing # we have shxdata/index 
+    read(shpdata, Shapefile.Handle, shxdata)
+  else
+    read(shpdata, Shapefile.Handle)
+  end 
+  if prjdata !== nothing
+    shp.crs = prjdata 
+  end 
+  return Shapefile.Table(shp, dbfdata)
+end 
+
+end
