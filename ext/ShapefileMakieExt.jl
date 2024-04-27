58160a11f95497dc908b44917f3d490f24cdcb22
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-04-27 08:13:58 -0400

    Update ShapefileMakieExt.jl

--- ext/ShapefileMakieExt.jl
+++ ext/ShapefileMakieExt.jl
@@ -10,7 +10,7 @@ GeoInterfaceMakie.@enable Shapefile.LinearRing
 Makie.plottype(tbl::Shapefile.Table) = Makie.plottype(Shapefile.shapes(tbl))
 Makie.plottype(shp::Shapefile.Handle) = Makie.plottype(Shapefile.shapes(shp))
 
-for T in (Any, Type{<:Makie.AbstractPlot}, Type{<:Makie.Poly}, Type{<:Makie.Lines}, Makie.PointBased)
+for T in (Any, Union{Type{Any}, Type{<:Makie.AbstractPlot}}, Type{<:Makie.Poly}, Type{<:Makie.Lines}, Makie.PointBased)
     @eval begin
         Makie.convert_arguments(t::$T, tbl::Shapefile.Table) =
             Makie.convert_arguments(t, Shapefile.shapes(tbl))
